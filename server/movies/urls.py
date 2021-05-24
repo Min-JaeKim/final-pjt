206935c1fab2b31d4ff73b5d5e4e62816534df4e
j-epa-peace <j.epa.peace@gmail.com>
2021-05-25 05:40:50 +0900

    j

--- server/movies/urls.py
+++ server/movies/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.movie_list_create),
     path('movie_list/', views.movie_list),
-    path('<str:movie_title>/likes/', views.likes),
+    path('<int:movie_id>/likes/', views.likes),
     path('<int:movie_id>/review_list/', views.review_list),
     path('<int:movie_id>/review_create/', views.review_create),
     path('<int:movie_id>/review/', views.review_update_delete),
