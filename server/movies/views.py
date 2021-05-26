3c7f392eb7e4ca7826a8ce5a91af24f33c920825
Min-JaeKim <minzminzm@gmail.com>
2021-05-26 19:27:56 +0900

    create_reply_backend

--- server/movies/views.py
+++ server/movies/views.py
@@ -19,7 +19,7 @@ from .serializers import (
     MovieListSerializer,
     CommentSerializer,
     ReplySerializer)
-from .models import Movie, Comment, MovieList, Rating, Favorite
+from .models import Movie, Comment, MovieList, Rating, Reply
 
 
 @api_view(['GET'])
@@ -95,68 +95,78 @@ def review_create(request, movie_id):
     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
-# @api_view(['PUT', 'DELETE'])
-# @authentication_classes([JSONWebTokenAuthentication])
-# @permission_classes([IsAuthenticated])
-# def review_update_delete(request):
-#     todo = get_object_or_404(Todo, pk=todo_pk)
-#     if not request.user.todos.filter(pk=todo_pk).exists():
-#         return Response({ 'detail' : '권한이 없습니다'}, status=status.HTTP_403_FORBIDDEN)
-#     if request.method == 'PUT':
-#         serializer = TodoSerializer(todo, data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#             return Response(serializer.data)
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def review_update_delete(request, movie_id, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if not request.user.todos.filter(pk=comment_pk).exists():
+        return Response({ 'detail' : '권한이 없습니다'}, status=status.HTTP_403_FORBIDDEN)
+    if request.method == 'PUT':
+        serializer = CommentSerializer(Comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
 
-#     elif request.method == 'DELETE':
-#         todo.delete()
-#         return Response({ 'id': todo_pk })
-
-
-# @api_view(['GET'])
-# def reply_list(request, movie_id, comment_id):
-#     comment = get_object_or_404(Comment, pk=comment_id)
-#     reply = comment.reply_set.all()
-#     # 이게 잘 작동이 되는 지 꼭 확인해 볼 것.
-#     # serializer = TodoSerializer(request.user.todos, many=True)
-#     return Response(reply, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response({ 'id': comment_pk })
 
 
-# @api_view(['POST'])
-# @authentication_classes([JSONWebTokenAuthentication])
-# @permission_classes([IsAuthenticated])
-# def reply_create(request, movie_id, comment_id):
-#     # # print(request)
-#     # if request.method == 'GET':
-
-#     # elif request.method == 'POST':
-#     #     serializer = TodoSerializer(data=request.data)
-#     #     if serializer.is_valid(raise_exception=True):
-#     #         serializer.save(user=request.user)
-#     #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     pass
-
-
-# def reply_update_delete(request):
-#     pass
+@api_view(['GET'])
+def reply_list(request, movie_id, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    serializers = ReplySerializer(comment.reply_set, many=True)
+    return Response(serializers.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 @authentication_classes([JSONWebTokenAuthentication])
 @permission_classes([IsAuthenticated])
-def movie_rate(request, username):
-    # 그런데 내가 지금 index.js의 movie 배열에 뭐가 들어오는지 확인을 못했다.
-    # movie = get_object_or_404(Movie, movie_id=request.data.movie.movie_id)
-    # 만약 movie 객체가 다 들어온다면
-    movie = request.data.movie
-    user = get_user_model().objects.get(username=username)
-    serializers = RatingSerializer(data=request.data.rate)
-    if serializers.is_valid():
-        # foreign key 라 객체 전부를..
-        serializers.save(movie=movie, user=user)
-        # 어차피 index.js에 모두 정의되어 있으므로 data를 따로 보내줄 필요는 없겠군요
+def reply_create(request, movie_id, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    serializer = ReplySerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(comment=comment, user=request.user)
         return Response(status=status.HTTP_201_CREATED)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def reply_update_delete(request, movie_id, comment_pk, reply_pk):
+    reply = get_object_or_404(Reply, pk=reply_pk)
+    if not request.user.todos.filter(pk=reply_pk).exists():
+        return Response({ 'detail' : '권한이 없습니다'}, status=status.HTTP_403_FORBIDDEN)
+    if request.method == 'PUT':
+        serializer = ReplySerializer(Reply, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        reply.delete()
+        return Response({ 'id': reply_pk })
+
+
+# @api_view(['POST'])
+# @authentication_classes([JSONWebTokenAuthentication])
+# @permission_classes([IsAuthenticated])
+# def movie_rate(request, movie_id, username):
+#     if request.user.todos.filter(pk=reply_pk).exists():
+#     # 그런데 내가 지금 index.js의 movie 배열에 뭐가 들어오는지 확인을 못했다.
+#     # movie = get_object_or_404(Movie, movie_id=request.data.movie.movie_id)
+#     # 만약 movie 객체가 다 들어온다면
+#     movie = request.data.movie
+#     user = get_user_model().objects.get(username=username)
+#     serializers = RatingSerializer(data=request.data.rate)
+#     if serializers.is_valid():
+#         # foreign key 라 객체 전부를..
+#         serializers.save(movie=movie, user=user)
+#         # 어차피 index.js에 모두 정의되어 있으므로 data를 따로 보내줄 필요는 없겠군요
+#         return Response(status=status.HTTP_201_CREATED)
+#     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 
